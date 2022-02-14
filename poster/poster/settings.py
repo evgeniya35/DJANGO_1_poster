b88e9178f535091faba321d6fd346d61770c29f7
evgeniya35 <evgeniya35@yandex.ru>
2022-02-14 11:45:03 +0300

    Add frontend

--- poster/poster/settings.py
+++ poster/poster/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'poster',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'poster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- poster/poster/views.py
+++ poster/poster/views.py
@@ -1,16 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def show_index(request):
-    return HttpResponse(
-        '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Стартовая</title>
-</head>
-<body>
-    <h1>Здесь будет карта</h1>
-</body>
-</html>
-        '''
-    )
+    context = {}
+    return render(request, 'index.html', context)
+
+
