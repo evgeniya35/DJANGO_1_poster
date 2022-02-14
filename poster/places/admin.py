583e65169a866e1bc0431f6c792698e53d92c389
evgeniya35 <evgeniya35@yandex.ru>
2022-02-14 13:31:51 +0300

    Add Excursion model

--- poster/places/admin.py
+++ poster/places/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from places.models import Excursion
 
 # Register your models here.
+
+admin.site.register(Excursion)
--- poster/places/models.py
+++ poster/places/models.py
@@ -1,5 +1,35 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
-print('123')
+class Excursion(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Название'
+    )
+    description_short = models.TextField(
+        verbose_name='Краткое описание',
+        blank=True,
+        default=''
+    )
+    description_long = models.TextField(
+        verbose_name='Полное описание',
+        blank=True,
+        default=''
+    )
+    lat = models.FloatField(
+        verbose_name='Широта',
+        default= 55
+    )
+    lon = models.FloatField(
+        verbose_name='Долгота',
+        default=37
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Экскурсия'
+        verbose_name_plural = 'Экскурсии'
