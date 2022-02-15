537d50417e7d87261c05b63682fe49f1af31adfb
evgeniya35 <evgeniya35@yandex.ru>
2022-02-15 14:38:37 +0300

    Add API for place.data to JSON

--- poster/poster/urls.py
+++ poster/poster/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
-from .views import show_index
+from .views import show_index, places
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_index)
+    path('', show_index),
+    path('places/<int:place_id>/', places),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- poster/poster/views.py
+++ poster/poster/views.py
@@ -1,38 +1,44 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from places.admin import Excursion
+from pprint import pprint
 
 def show_index(request):
-    excursions = {
-        "type": "FeatureCollection",
-        "features": [
+    places = Excursion.objects.all()
+    features = []
+    for place in places:
+        features.append(
             {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [place.lon, place.lat]
                 },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "static/places/roofs24.json"
+                'properties': {
+                    'title': place.title,
+                    'placeId': place.id,
+                    'detailsUrl': 'static/places/moscow_legends.json'
                 }
             }
-        ]
+        )
+    excursions = {
+        'type': 'FeatureCollection',
+        'features': features
     }
-
     context = {'excursions': excursions}
     return render(request, 'index.html', context)
 
 
+def places(request, place_id=1):
+    place = get_object_or_404(Excursion, pk=place_id)
+    place_context = {
+            'title': place.title,
+            'imgs': [img.photo.url for img in place.exc_photos.all().order_by('sort_index')],
+            'description_short': place.description_short,
+            'description_long': place.description_long,
+            'coordinates': {
+                'lat': place.lat,
+                'lng': place.lon
+            }
+        }
+    return JsonResponse(place_context, json_dumps_params={'ensure_ascii': False})
