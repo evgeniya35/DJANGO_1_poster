453390e57261784457a2d73d031b3fbbe37d3a2b
evgeniya35 <evgeniya35@yandex.ru>
2022-02-15 15:52:07 +0300

    Add inline form to Excursion for Image

--- poster/places/admin.py
+++ poster/places/admin.py
@@ -3,5 +3,19 @@ from places.models import Excursion, Image
 
 # Register your models here.
 
-admin.site.register(Excursion)
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+class ExcursionAdmin(admin.ModelAdmin):
+    # форма элемента
+    fieldsets = [
+        (None,         {'fields': ['title']}),
+        ('Описание',   {'fields': ['description_short', 'description_long'], 'classes': ['collapse']}),
+        ('Координаты', {'fields': ['lat', 'lon'], 'classes': ['collapse']}),
+    ]
+    inlines = [ImageInline]
+
+
+admin.site.register(Excursion, ExcursionAdmin)
 admin.site.register(Image)
