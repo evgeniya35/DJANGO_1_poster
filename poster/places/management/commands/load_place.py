def855fb2664d0bfb361c238ffdbb02fb0ccf99e
evgeniya35 <evgeniya35@yandex.ru>
2022-02-28 08:50:08 +0300

    Added error handling KeyError

--- poster/places/management/commands/load_place.py
+++ poster/places/management/commands/load_place.py
@@ -1,8 +1,7 @@
 import requests
 
-from django.db import transaction
 from django.core.management.base import BaseCommand
-from django.core.files.images import ImageFile
+from django.conf import settings
 from urllib.parse import urlparse
 from io import BytesIO
 
@@ -27,28 +26,35 @@ class Command(BaseCommand):
             response = requests.get(url)
             response.raise_for_status()
             place_content = response.json()
-            place_content.setdefault('coordinates', {'lat': 55, 'lng': 37}),
-            excursion, created = Excursion.objects.update_or_create(
-                title=place_content.get('title'),
-                defaults={
-                    'description_short': place_content.get('description_short'),
-                    'description_long': place_content.get('description_long'),
-                    'lat': place_content['coordinates'].get('lat'),
-                    'lon': place_content['coordinates'].get('lng')
-                }
-            )
-            self.stdout.write(f'Load {excursion.title} {created}')
-            for num, img in enumerate(place_content.get('imgs'), start=1):
-                response = requests.get(img)
-                response.raise_for_status()
-                img_name = urlparse(img).path.split('/')[-1]
-                image, _created = Image.objects.get_or_create(
-                    sort_index=num,
-                    excursion=excursion,
+            try:
+                excursion, created = Excursion.objects.update_or_create(
+                    title=place_content['title'],
+                    defaults={
+                        'description_short': place_content.get('description_short', ''),
+                        'description_long': place_content.get('description_long', ''),
+                        'lat': place_content['coordinates']['lat'],
+                        'lon': place_content['coordinates']['lng']
+                    }
                 )
-                image.photo.save(
-                    f'{excursion.id}_{img_name}',
-                    BytesIO(response.content),
-                    save=True
-                )
-                self.stdout.write(f'Load {image.photo.url}')
+                self.stdout.write(f'Load {excursion.title} {created}')
+                for num, img in enumerate(place_content.get('imgs'), start=1):
+                    img_name = urlparse(img).path.split('/')[-1]
+                    response = requests.get(img)
+                    response.raise_for_status()
+                    image, _created = Image.objects.get_or_create(
+                        sort_index=num,
+                        excursion=excursion,
+                    )
+                    image.photo.save(
+                        f'{excursion.id}_{img_name}',
+                        BytesIO(response.content),
+                        save=True
+                    )
+                    self.stdout.write(f'Load {image.photo.url}')
+            except KeyError as error:
+                self.stdout.write(f'Error load: {error}')
+                continue
+            except:
+                self.stdout.write('Error load')
+
+
